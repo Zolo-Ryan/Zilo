389d496ff93c08915f2bb2b67259e5dab4e739ad
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2024-03-25 02:27:00 +0530

    added a functionality to get windows size using ioctl(easy way)

--- kilo.c
+++ kilo.c
@@ -5,28 +5,40 @@
 #include<termios.h>
 #include<ctype.h>
 #include<stdlib.h>
+#include<sys/ioctl.h>
 
 /* defines */
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /* data */
-struct termios orig_termios;
+struct editorConfig{
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+struct editorConfig E;
 
 /* terminal */
 void enableRawMode(void); // enabling raw mode
 void disableRawMode(void); //disabling raw mode after program is exited;
 void die(const char*); // for error handling
 char editorReadKey(void); // waits for a keypress and return it
+int getWindowSize(int*,int*);
 
 /* output */
 void editorRefreshScreen(void); // to refresh the screen initially
+void editorDrawRows(void); // to draw tilde on the side of screen just like vim does
 
 /* input */
 void editorProcessKeypress(void); // waits for a keypress and then handles it
 
+/* init */
+void initEditor(void);
+
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
@@ -35,9 +47,34 @@ int main()
     return 0;
 }
 
+void initEditor(){
+    if(getWindowSize(&E.screenrows,&E.screencols) == -1) die("getWindowSize");
+}
+
+int getWindowSize(int *rows,int *cols){
+    struct winsize ws;
+
+    // input output control on stdout, terminal input output control get window size, structure
+    if(ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws) == -1 || ws.ws_col == 0)
+        return -1;
+    else{
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 void editorRefreshScreen(){
     write(STDOUT_FILENO,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
     write(STDOUT_FILENO,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
+    editorDrawRows();
+    write(STDOUT_FILENO,"\x1b[H",3);
+}
+void editorDrawRows(){
+    int y;
+    for(y = 0;y<E.screenrows;y++){
+        write(STDOUT_FILENO,"~\r\n",3);
+    }
 }
 
 void editorProcessKeypress(){
@@ -61,10 +98,10 @@ char editorReadKey(){
 }
 
 void enableRawMode(void){
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP);
     raw.c_cflag |= (CS8);
 
@@ -77,7 +114,7 @@ void enableRawMode(void){
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 void disableRawMode(void){
-    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios) == -1)
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios) == -1)
         die("tcsetattr");
 }
 
