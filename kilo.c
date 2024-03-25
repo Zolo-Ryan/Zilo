da1b46813779a167ec248ae94e2c777fe5558ce8
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2024-03-25 16:10:16 +0530

    Now cursor can be moved with the arrow keys

--- kilo.c
+++ kilo.c
@@ -11,9 +11,16 @@
 /* defines */
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
+enum editorKey{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 
 /* data */
 struct editorConfig{
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -42,7 +49,7 @@ void abFree(struct abuf *ab){
 void enableRawMode(void); // enabling raw mode
 void disableRawMode(void); //disabling raw mode after program is exited;
 void die(const char*); // for error handling
-char editorReadKey(void); // waits for a keypress and return it
+int editorReadKey(void); // waits for a keypress and return it
 int getWindowSize(int*,int*);
 int getCursorPosition(int*,int*);
 
@@ -52,6 +59,7 @@ void editorDrawRows(struct abuf *ab); // to draw tilde on the side of screen jus
 
 /* input */
 void editorProcessKeypress(void); // waits for a keypress and then handles it
+void editorMoveCursor(int);
 
 /* init */
 void initEditor(void);
@@ -68,11 +76,152 @@ int main()
     return 0;
 }
 
-
+/** init **/
 void initEditor(){
+    E.cx = 0; // col(horizontal coordinate)
+    E.cy = 0; // row(vertical coordinate)
+
     if(getWindowSize(&E.screenrows,&E.screencols) == -1) die("getWindowSize");
 }
 
+/** output **/
+void editorRefreshScreen(){
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab,"\x1b[?25l",6); // hides the cursor
+    // abAppend(&ab,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
+    abAppend(&ab,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
+    
+    editorDrawRows(&ab);
+    
+    char buf[32];
+    snprintf(buf,sizeof(buf),"\x1b[%d;%dH",E.cy+1,E.cx+1); // moves the cursor to its correct location
+    abAppend(&ab,buf,strlen(buf));
+    
+    abAppend(&ab,"\x1b[?25h",6); // makes the cursor visible
+
+    write(STDOUT_FILENO,ab.b,ab.len);
+    abFree(&ab);
+}
+void editorDrawRows(struct abuf *ab){
+    int y;
+    for(y = 0;y<E.screenrows;y++){
+        if(y == E.screenrows / 3){
+            char welcome[80];
+            int welcomelen = snprintf(welcome,sizeof(welcome),"Kilo editor -- version %s",KILO_VERSION);
+
+            if(welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if(padding){
+                abAppend(ab,"~",1);
+                padding--;
+            }
+            while(padding--) abAppend(ab," ",1);
+            abAppend(ab,welcome,welcomelen);
+        }
+        else{
+            abAppend(ab,"~",1);
+        }
+        abAppend(ab,"\x1b[K",3); // K (erase in line). erases part of current line.2 for whole, 1 for left of cursor, 0 (default) for right
+        if(y < E.screenrows - 1){
+            abAppend(ab,"\r\n",2);
+        }
+    }
+}
+
+/** input **/
+void editorProcessKeypress(){
+    int c = editorReadKey();
+
+    switch(c){
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
+            write(STDOUT_FILENO,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
+            exit(EXIT_SUCCESS);
+            break;
+        
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
+    }
+}
+void editorMoveCursor(int key){
+    switch(key){
+        case ARROW_LEFT:
+            if(E.cx > 0)
+                E.cx--;
+            break;
+        case ARROW_RIGHT:
+            if(E.cx < E.screencols - 1)
+                E.cx++;
+            break;
+        case ARROW_UP:
+            if(E.cy > 0)
+                E.cy--;
+            break;
+        case ARROW_DOWN:
+            if(E.cy < E.screenrows - 1)
+                E.cy++;
+            break;
+    }
+}
+
+/** terminal **/
+int editorReadKey(){
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO,&c,1)) != 1){
+        if(nread == -1 && errno != EAGAIN) die("read");
+    }
+
+    if(c == '\x1b'){
+        char seq[3];
+        
+        if(read(STDIN_FILENO,&seq[0],1) != 1) return '\x1b'; // => only escape was pressed
+        if(read(STDIN_FILENO,&seq[1],1) != 1) return '\x1b'; // => some other escape seq was pressed but we just return escape
+
+        if(seq[0] == '['){
+            switch(seq[1]){
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+
+        return '\x1b'; // <esc>? was pressed. here ? denotes idk what was the character was after <esc> but i am sure it is not [
+    }
+    else
+        return c;
+}
+void enableRawMode(void){
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
+
+    struct termios raw = E.orig_termios;
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP);
+    raw.c_cflag |= (CS8);
+
+    raw.c_iflag &= ~(IXON | ICRNL); // input flag, XON to turn off output flow(s,q), turns off carriage return fixes m
+    raw.c_oflag &= ~(OPOST); // turns of translation on the output side: carriage return won't automatically be added
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // local flags, turn of echo, canonical or cooked mode(\n req for input to be passed into program), turn off v, SIGINT and SIGTSTP signals(c,z)
+    raw.c_cc[VMIN] = 0; // minimum number of bytes needed before read can return
+    raw.c_cc[VTIME] = 1; // max amt of time to wait before read returns. 1 => 1/10th of a second
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+void disableRawMode(void){
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios) == -1)
+        die("tcsetattr");
+}
+void die(const char* message){
+    write(STDOUT_FILENO,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
+    write(STDOUT_FILENO,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
+    perror(message);
+    exit(EXIT_FAILURE);
+}
 int getCursorPosition(int *rows,int *cols){
     char buf[32];
     unsigned int i = 0;
@@ -103,89 +252,4 @@ int getWindowSize(int *rows,int *cols){
         *rows = ws.ws_row;
         return 0;
     }
-}
-
-void editorRefreshScreen(){
-    struct abuf ab = ABUF_INIT;
-    abAppend(&ab,"\x1b[?25l",6);
-    // abAppend(&ab,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
-    abAppend(&ab,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
-    editorDrawRows(&ab);
-    abAppend(&ab,"\x1b[H",3);
-    abAppend(&ab,"\x1b[?25h",6);
-
-    write(STDOUT_FILENO,ab.b,ab.len);
-    abFree(&ab);
-}
-void editorDrawRows(struct abuf *ab){
-    int y;
-    for(y = 0;y<E.screenrows;y++){
-        if(y == E.screenrows / 3){
-            char welcome[80];
-            int welcomelen = snprintf(welcome,sizeof(welcome),"Kilo editor -- version %s",KILO_VERSION);
-
-            if(welcomelen > E.screencols) welcomelen = E.screencols;
-            int padding = (E.screencols - welcomelen) / 2;
-            if(padding){
-                abAppend(ab,"~",1);
-                padding--;
-            }
-            while(padding--) abAppend(ab," ",1);
-            abAppend(ab,welcome,welcomelen);
-        }
-        else
-            abAppend(ab,"~",1);
-        abAppend(ab,"\x1b[K",3); // K (erase in line). erases part of current line.2 for whole, 1 for left of cursor, 0 (default) for right
-        if(y < E.screenrows - 1){
-            abAppend(ab,"\r\n",2);
-        }
-    }
-}
-
-void editorProcessKeypress(){
-    char c = editorReadKey();
-
-    switch(c){
-        case CTRL_KEY('q'):
-            write(STDOUT_FILENO,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
-            write(STDOUT_FILENO,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
-            exit(EXIT_SUCCESS);
-            break;
-    }
-}
-char editorReadKey(){
-    int nread;
-    char c;
-    while((nread = read(STDIN_FILENO,&c,1)) != 1){
-        if(nread == -1 && errno != EAGAIN) die("read");
-    }
-    return c;
-}
-
-void enableRawMode(void){
-    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
-    atexit(disableRawMode);
-
-    struct termios raw = E.orig_termios;
-    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP);
-    raw.c_cflag |= (CS8);
-
-    raw.c_iflag &= ~(IXON | ICRNL); // input flag, XON to turn off output flow(s,q), turns off carriage return fixes m
-    raw.c_oflag &= ~(OPOST); // turns of translation on the output side: carriage return won't automatically be added
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // local flags, turn of echo, canonical or cooked mode(\n req for input to be passed into program), turn off v, SIGINT and SIGTSTP signals(c,z)
-    raw.c_cc[VMIN] = 0; // minimum number of bytes needed before read can return
-    raw.c_cc[VTIME] = 1; // max amt of time to wait before read returns. 1 => 1/10th of a second
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
-}
-void disableRawMode(void){
-    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios) == -1)
-        die("tcsetattr");
-}
-
-void die(const char* message){
-    write(STDOUT_FILENO,"\x1b[2J",4); // \x1b is escape(27). escape sequence start with '<esc>[', here we are running J command and giving a parameter of '2', hence 4 bits
-    write(STDOUT_FILENO,"\x1b[H",3); // takes two arguments, 80x24 h toh <esc>[12;40H for center
-    perror(message);
-    exit(EXIT_FAILURE);
 }
